18b0740014e4a09f893886f8ebe1b33b4a50c47e
Leonardo <leocasseb713@gmail.com>
2025-05-02 20:58:17 -0300

    alterações que permitem que o jogo continue

--- app.js
+++ app.js
@@ -1,6 +1,39 @@
 alert('Boas vindas ao jogo do número secreto!');
-let numeroSecreto = 5;
-let chute = prompt('Escolha um número entre 1 e 10!');
-if (chute == numeroSecreto) {
-    alert('Você acertou!')
+let numeroSecreto = prompt('Escolha um número secreto!');
+let chute
+while (chute != numeroSecreto) {
+    chute = prompt('chute um valor!');
+    if (chute == numeroSecreto) {
+        alert('Você acertou! O número secreto é ' + numeroSecreto);
+    } else {
+        if (chute > numeroSecreto) {
+            alert('Errou! O número secreto é menor que ' + chute);
+        }
+    else { 
+        if (chute < numeroSecreto) 
+            alert('Errou! O número secreto é maior que ' + chute);
+        }
+    };
 };
+let continuar = prompt('Deseja continuar jogando? (sim/não)');
+while (continuar != 'sim' && continuar != 'não') {
+    continuar = prompt('Digite sim ou não!');
+}
+while (continuar == 'sim'){
+    numeroSecreto = prompt('Escolha um número secreto!');
+    chute = null
+    while (chute != numeroSecreto) {
+        chute = prompt('Chute um valor!');
+        if (chute == numeroSecreto) {
+            alert('Você acertou! O número secreto é ' + numeroSecreto);
+            continuar = prompt('Deseja continuar jogando? (sim/não)');
+        } else {
+            if (chute > numeroSecreto) {
+                alert('Errou! O número secreto é menor que ' + chute);
+            } else { 
+                if (chute < numeroSecreto) 
+                    alert('Errou! O número secreto é maior que ' + chute);
+            }
+        };
+    };
+}
