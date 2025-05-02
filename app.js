8a6ae69a3c2b4c6d9c46aaf3fff8618ee14e2018
Leonardo <leocasseb713@gmail.com>
2025-05-02 20:13:58 -0300

    treinando git

---
+++ app.js
@@ -0,0 +1,6 @@
+alert('Boas vindas ao jogo do número secreto!');
+let numeroSecreto = 5;
+let chute = prompt('Escolha um número entre 1 e 10!');
+if (chute == numeroSecreto) {
+    alert('Você acertou!')
+};
